baf6eba2a8055c7c441974ed1c789ec7c565e727
Rish-01 <rishab2001rs@gmail.com>
2022-10-06 23:32:21 +0530

    Script to extract image from a single file

--- read_image.py
+++ read_image.py
@@ -2,7 +2,7 @@ import pds4_tools
 from PIL import Image
 
 # Read the file
-struct_list = pds4_tools.read('local-path-to-extracted-file/data/calibrated/20220129/xml-file')
+struct_list = pds4_tools.read('local-path-to-extracted-file/data/calibrated/folder-number/xml-file')
 # Extract the image from the file
 im = struct_list[0].data
 # Slice part of the image array
